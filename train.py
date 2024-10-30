a4aef1558d3f36fc5bd54867b03e2b5ff44881fb
jerrystamm <144961621+jerrystamm@users.noreply.github.com>
2024-10-29 20:03:01 -0400

    Modified train.py to continue training if a model already exists

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from gymnasium.wrappers import GrayScaleObservation
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import VecFrameStack, SubprocVecEnv
 from src.YoshiEnv import YoshiEnv
+import os
 
 def make_env():
     env = YoshiEnv()
@@ -15,10 +16,17 @@ def train_model():
     num_envs = 1
     env = SubprocVecEnv([make_env for _ in range(num_envs)])
     env = VecFrameStack(env, 4, channels_order='last')
-
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="./logs/", device='cuda')
     
-    model.learn(total_timesteps=3000000)
+    model_path = "yoshi_train.zip"  
+
+    if os.path.exists(model_path):
+        print("continue")
+        model = PPO.load(model_path, env = env, verbose = 1, tensorboard_log = "./logs/", device = 'cuda')
+    else:
+        print("no")
+        model = PPO("CnnPolicy", env, verbose = 1, tensorboard_log = "./logs/", device = 'cuda')
+
+    model.learn(total_timesteps=1000000)
     model.save("yoshi_train")
 
     env.close()
